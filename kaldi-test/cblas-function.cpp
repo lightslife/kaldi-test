a569cc7be025be62e856f8becb4c743f11f11f51
guangxuhuang <guangxuhuang@hotmail.com>
2018-06-05 22:42:50 +0800

    add cblas

--- kaldi-test/cblas-function.cpp
+++ kaldi-test/cblas-function.cpp
@@ -56,8 +56,49 @@ namespace kaldi {
 		return 0;
 	}
 
+	int cblas_Xcopy(int numCols, const float * srcData, int a, float *data, int b) {
+		//TODO
+		return 0;
+	}
+	int cblas_Xcopy(int numCols, const double * srcData, int a, double *data, int b) {
+		//TODO
+		return 0;
+	}
 
+	int cblas_Xdot(int acols, const float*aData, int a, float * bData, int bStride) {
+		//TODO
+		return 0;
+	}
+	int cblas_Xdot(int acols, const double*aData, int a, double * bData, int bStride) {
+		//TODO
+		return 0;
+	}
 
+	int cblas_Xgbmv(int kNoTrans, int aDim, int rDim, int a, int b, float alpha, const float * aData, int c, const float * rData, int d, float beta, float * data_, int e) {
+		//TODO
+		return 0;
+	}
 
+	int cblas_Xgbmv(int kNoTrans, int aDim, int rDim, int a, int b, double alpha, const double * aData, int c, const double * rData, int d, double beta, double *data_, int e) {
+		//TODO
+		return 0;
+	}
+	int cblas_Xger(int aDim, int rbDim, float alpha, const float * aData, int a, const float * rbData, int b, float *data_, int stride) {
+		//TODO
+		return 0;
+	}
+	int cblas_Xger(int aDim, int rbDim, double alpha, const double * aData, int a, const double * rbData, int b, double *data_, int stride) {
+		//TODO
+		return 0;
+	}
+ 
+	int cblas_Xdot(int aDim, const float * aData, int a, const float *bData, int b){
+		//TODO
+		return 0;
+}
+	int cblas_Xdot(int aDim, const double * aData, int a, const double *bData, int b){
+		//TODO
+		return 0;
+	}
 
 }//kaldi
